0342b66a21bfbce2815f9419d47c876326a8100f
Devin Wixon <dwixon@frogstreet.com>
2025-08-10 19:17:55 -0500

    district_programs stage builds

---
+++ models/staging/taco/stg_taco__districts_programs.sql
@@ -0,0 +1,19 @@
+with
+source_table as (
+    select * from {{ source('taco', 'raw_taco__districts_programs') }}
+),
+
+final as (
+    select
+        -- no key in source; generating
+        {{ dbt_utils.generate_surrogate_key(
+            ['district_id', 'program_id']) }} as district_program_id_sk ,
+        district_id::int as district_id,
+        program_id::int as program_id,
+        enabled::boolean as is_enabled,
+        -- change unix timestamp (which is always 5pm) to date
+        date(expiration_date::int) as expiration_date_dt
+    from source_table
+)
+
+select * from final
