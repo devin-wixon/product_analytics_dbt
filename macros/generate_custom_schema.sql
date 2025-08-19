1f6222090d32ca3ae0ca77cf1c428afc1fd0d899
Devin Wixon <dwixon@frogstreet.com>
2025-08-18 21:58:48 -0500

    change custom schema macro to build prod in the right schemas

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if target.name == 'prod' -%}
+    {%- if target.name != 'Development' -%}
 
         {{ custom_schema_name | trim }} 
 
