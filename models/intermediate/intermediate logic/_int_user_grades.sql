fedf75d0bfd07b75ce049ddf780426bc2f96c162
Devin Wixon <dwixon@frogstreet.com>
2025-08-19 14:15:22 -0500

    obt_events exclude columns

---
+++ models/intermediate/intermediate logic/_int_user_grades.sql
@@ -0,0 +1,5 @@
+{{ config(
+    enabled = False
+)}}
+
+-- placeholder for user_id | grades logic that integrates grades and other grades, once available
---
+++ models/marts/rpt_obt_events.sql
@@ -0,0 +1,115 @@
+
+-- using exclude rather than explicit columns
+-- to ensure new columns are included
+with users as (
+    select
+        * exclude (
+            user_sourced_id,
+            is_disable_auto_sync,
+            is_manually_added
+        )
+    from
+        {{ ref('dim_users_current') }}
+),
+districts as (
+    select
+        * exclude (
+          district_address,
+          district_city,
+          district_state_international,
+          is_district_enabled,
+          district_website_slug,
+          district_settings,
+          district_general_settings,
+          is_district_auto_sync,
+          district_sourced_id,
+          district_identifier,
+          -- district_sales_force_id,
+          -- district_mas_id,
+          district_sage_id,
+          district_last_sync_utc,
+          district_last_modified_at_utc,
+          roster_file_created_at_utc,
+          auto_rostering_checked_at_utc
+        )
+    from
+        {{ ref('dim_districts_current') }}
+),
+
+programs as (
+    select
+        * exclude(
+            custom_banner,
+            clone_status,
+            deleted_date
+        )
+    from
+        {{ ref('dim_programs') }}
+),
+
+resources as (
+    select
+        * exclude(
+            -- resource_code,
+            resource_description,
+            file_url,
+            section_title,
+            link,
+            order_number,
+            thumbnail_mobile_url,
+            thumbnail_web_url,
+            uuid,
+            physical_page_number,
+            fts_title,
+            -- TAG TO DO determine how populated;
+            -- focus_area is null when focus_area_id is not
+            focus_area
+        )
+    from
+        {{ ref('dim_resources_current') }}
+),
+
+-- columns may be generated through pivots in int_ layer
+events as (
+  select
+    * exclude(
+        server_timestamp,
+        event_value,
+        server_event_date,
+        event_value_joins_to,
+        event_value_not_id
+    )
+  from
+    {{ ref('fct_events') }}
+), 
+joined as (
+  select
+    events.*,
+    users.* exclude (user_id),
+    districts.* exclude (district_id),
+    programs.* exclude (program_id),
+    -- add focus_area table later as needed
+    resources.* exclude (resource_id, program_id, focus_area_id)
+from
+  events 
+  left join users on events.user_id = users.user_id
+  left join districts on users.district_id = districts.district_id
+  left join programs on events.program_id = programs.program_id
+  left join resources on events.resource_id = resources.resource_id
+),
+
+final as
+(select
+  *
+from
+  joined
+{%- if target.name == 'Development' %}
+  -- Limit number of rows in development environment
+  limit 100
+{%- endif -%}
+)
+
+select 
+  *
+from
+  final
