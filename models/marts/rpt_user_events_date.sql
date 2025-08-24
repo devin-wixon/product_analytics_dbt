e7bb5bf55d43009da250784016e9b99399b0f79f
Devin Wixon <dwixon@frogstreet.com>
2025-08-24 11:04:04 -0500

    add event_category counts and booleans to user_events_date

--- models/marts/rpt_user_events_date.sql
+++ models/marts/rpt_user_events_date.sql
@@ -29,15 +29,19 @@ resources as (
 
 user_daily_activity as (
     select
-        users.*,
-        events.client_event_date,
-        
+        -- aggregation: user (other user columns are 1:1), date
+        users.user_id,
+        users.user_role,
+        users.user_grades,
+        users.user_other_grades
+        users.events.client_event_date,
+            
         -- Program metrics
-        count(distinct events.program_id) as n_programs_accessed,
+        count(distinct events.program_id) as n_programs_accessed_per_user_date,
 
         -- Resource metrics  
-        count(distinct events.resource_id) as n_resources_accessed,
-        -- TAG OPTIMIZE 
+        count(distinct events.resource_id) as n_resources_accessed_per_user_date,
+        -- TAG OPTIMIZE OPPORTUNITY
         -- test performance vs dbt_utils.pivot() or Snowflake dynamic pivot 
         {% set resource_types = dbt_utils.get_column_values(
             table=ref('dim_resources_current'),
@@ -48,17 +52,30 @@ user_daily_activity as (
 
         {% for resource_type in resource_types  %}
         count(distinct case when resources.resource_type = '{{ resource_type }}'
-            then resources.resource_id end) as n_{{ resource_type }}_accessed,
+            then resources.resource_id end) as n_{{ resource_type }}_accessed_per_user_date,
         {% endfor %}
         
         -- Event metrics
         count(*) as n_total_events,
 
-        -- TAG TO DO add logic using the loops as in int_events_enriched for event_category values in int_events_enriched columns
+        {%- set event_categories = dbt_utils.get_column_values(
+            table=ref('seed_event_log_metadata'),
+            column='event_category',
+            where="event_category is not null and event_category != ''",
+            order_by='event_category'
+            )-%}
+        {%- if event_categories -%}
+            {%- for category in event_categories -%}
+            ,
+            -- counts of event_category events
+            count(distinct case when event_category = '{{ category }}'
+                then events.event_id end) as n_{{ category }}_events_per_user_date,
 
-        -- boolean metrics
-        -- add logic  using the loops as in int_events_enriched for event_category values in int_events_enriched columns
-
+            -- boolean: has event_category
+            max(event_category = '{{ category }}'
+                as user_date_has_{{ category }}_event_per_user_date )
+            {%- endfor %}
+        {%- endif %}
 
         -- Session metrics: later
         
@@ -76,10 +93,10 @@ user_daily_activity as (
         events.resource_id = resources.resource_id
     group by 1,2,3,4,5
 
-{%- if target.name == 'Development' %}
-    -- Limit number of rows in development environment
-    limit 100000
-{%- endif -%}
+    {%- if target.name == 'Development' %}
+        -- Limit number of rows in development environment
+        limit 1000
+    {%- endif -%}
 ),
 
 final as (
