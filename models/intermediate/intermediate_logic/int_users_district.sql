16f51cc851b074048213c5ba7f8a043a3529fd85
Devin Wixon <dwixon@frogstreet.com>
2025-09-07 17:44:18 +0000

    lint and fix district_settings and users_district

--- models/intermediate/intermediate_logic/int_users_district.sql
+++ models/intermediate/intermediate_logic/int_users_district.sql
@@ -1,31 +1,29 @@
-with 
+with
 
 -- a user may have many records in the staging model because it is a snapshot with date-based change capture
 -- a user will only ever have one district_id
 -- and this chooses administrator over teacher if there are multiple roles for a user
-users_district_role as 
-(select
-    user_id,
-    district_id,
-    min(user_role) as user_highest_role,
-    min(email_sent_utc) as first_email_sent_utc
-from
-    {{ ref('stg_taco__users') }}
-where
-    user_role != 'student'
-group by
-    user_id,
-    district_id
+users_district_role as (
+    select
+        user_id,
+        district_id,
+        min(user_role) as user_highest_role,
+        min(email_sent_utc) as first_email_sent_utc
+    from
+        {{ ref('stg_taco__users') }}
+    where
+        user_role != 'student'
+    group by
+        user_id,
+        district_id
 ),
 
-final as
-(select
-    *
-from
-    users_district_role
-) 
+final as (
+    select *
+    from
+        users_district_role
+)
 
-select
-    *
+select *
 from
     final
