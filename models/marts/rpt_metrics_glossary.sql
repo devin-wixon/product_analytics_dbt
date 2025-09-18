41b7d75ec729d0430f903feb1ef674a0f73aaa41
Devin Wixon <dwixon@frogstreet.com>
2025-09-18 16:46:17 -0500

    working glossary and simple measures to metrics; add time grains for Tableau

--- models/marts/rpt_metrics_glossary.sql
+++ models/marts/rpt_metrics_glossary.sql
@@ -1,10 +1,18 @@
-{{ config(materialized='table') }}
-
 {%- set metrics_list = [] -%}
 
 {%- for metric_name, metric in graph.metrics.items() -%}
-    {%- set clean_description = (metric.description | replace("'", "") | replace('"', '') | replace('\n', ' ') | replace('\r', '') | truncate(200)) if metric.description else '' -%}
-    {%- set clean_name = metric.name | replace("'", "") | replace('"', '') -%}
+    {%- set clean_description = (
+            metric.description 
+                | replace("'", "") 
+                | replace('"', '') 
+                | replace('\n', ' ') 
+                | replace('\r', '') 
+                | truncate(200)) if metric.description 
+    else '' -%}
+    {%- set clean_name = metric.name 
+        | replace("'", "") 
+        | replace('"', '') 
+    -%}
     {%- do metrics_list.append({
         'name': clean_name,
         'description': clean_description,
@@ -18,4 +26,9 @@ SELECT * FROM (
         ('{{ metric.name }}', '{{ metric.description }}', '{{ metric.type }}', CURRENT_TIMESTAMP())
         {%- if not loop.last -%},{%- endif -%}
     {%- endfor %}
-) AS metrics_glossary(metric_name, metric_description, metric_type, last_updated)
+) AS metrics_glossary(
+    metric_name, 
+    metric_description, 
+    metric_type, 
+    last_updated
+    )
