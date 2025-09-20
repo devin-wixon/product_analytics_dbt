983b32cdffbf329370ab7dc2b95c2f4a2823b20c
Devin Wixon <dwixon@frogstreet.com>
2025-09-19 20:57:42 -0500

    add district to fct_events for sl

--- models/intermediate/facts/fct_events.sql
+++ models/intermediate/facts/fct_events.sql
@@ -6,11 +6,26 @@ events as (
         {{ ref("int_events_enriched") }}
 ),
 
+-- a user can never switch districts, so this is a 1:1 mapping that is not date-specific
+-- district_id needs to be in fct_events for semantic modeling
+    users_district as (
+        select
+            user_id,
+            district_id
+        from
+            {{ ref("int_users_district") }}
+    ),
+
 final as (
-    select *
+    select
+        events.*,
+        users_district.district_id
     from
         events
-)
+    left join users_district
+        on events.user_id = users_district.user_id
+    )
+
 
 select *
 from
--- models/marts/rpt_obt_events.sql
+++ models/marts/rpt_obt_events.sql
@@ -6,11 +6,11 @@ with
 events as (
   select
   -- use * and exclude, not explicit select; columns may be generated through pivots in int_ layer
-    * 
-    --   exclude(
-    --     server_timestamp,
-    --     server_event_date,
-    -- )
+  -- exclude district_id; if not sl, will remove dep on int_user_districts in fct_events and use date-based user attributes
+    *
+      exclude(
+        district_id
+    )
   from
     {{ ref('fct_events') }}
   {%- if target.name == 'Development' %}
