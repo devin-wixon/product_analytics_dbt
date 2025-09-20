1cead4ac1d2a63cd19871e7b1abe90731eb965c1
Devin Wixon <dwixon@frogstreet.com>
2025-09-20 18:51:06 -0500

    revise tableau_time_grains_dims for tableau parameterization

--- models/marts/bi_sl_exports_as_sources/tableau_time_grains_dims.sql
+++ models/marts/bi_sl_exports_as_sources/tableau_time_grains_dims.sql
@@ -2,41 +2,49 @@
 
   month_program as (
       select
-          event__metric_time_month__month as month,
+          event__metric_time_month__month as date_spine,
           'month' as time_grain,
           'program' as dim_set,
-          program__program_name as program_name,
-          cast(null as varchar) as resource_type_name,
+          program__program_name as dim_a_value,
+          cast(null as varchar) as dim_b_value,
           n_active_users,
           n_events
       from
           {{ source('exports', 'qexptbl_month_program') }}
+    where
+        program__program_name is not null
+
   ),
 
   month_resource_type as (
       select
-          event__metric_time_month__month as month,
+          event__metric_time_month__month as date_spine,
           'month' as time_grain,
           'resource_type' as dim_set,
-          cast(null as varchar) as program_name,
-          resource__resource_type as resource_type_name,
+          resource__resource_type as dim_a_value,
+          cast(null as varchar) as dim_b_value,
           n_active_users,
           n_events
       from
           {{ source('exports', 'qexptbl_month_resource_type') }}
+    where
+        resource__resource_type is not null
   ),
 
   month_program_resource_type as (
       select
-          event__metric_time_month__month as month,
+          event__metric_time_month__month as date_spine,
           'month' as time_grain,
           'program__resource_type' as dim_set,
-          program__program_name as program_name,
-          resource__resource_type as resource_type_name,
+          program__program_name as dim_a_value,
+          resource__resource_type as dim_b_value,
           n_active_users,
           n_events
       from
           {{ source('exports', 'qexptbl_month_program_resource_type') }}
+    where
+         -- they can't both be null
+        program__program_name is not null or resource__resource_type is not null
   ),
 
   final as (
