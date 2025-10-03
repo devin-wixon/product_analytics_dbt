904d5aafa4fee09732a8965138c88aa55baf92cf
Devin Wixon <dwixon@frogstreet.com>
2025-10-03 10:25:26 -0500

    add first usage date

--- models/marts/rpt_wau_mau_by_user.sql
+++ models/marts/rpt_wau_mau_by_user.sql
@@ -20,6 +20,15 @@ with events_dims_daily as(
     from    
         {{ ref('rpt_obt_events_dims_daily') }}
 ),
+user_fed as(
+    select 
+        user_id,
+        min(server_event_date) as user_first_event_date
+    from 
+        events_dims_daily
+    group by user_id
+),
+
 datespine as (
 select
     date_day,
@@ -34,12 +43,17 @@ final as (
         datespine.date_day,
         datespine.mau_lookback_start_date,
         datespine.wau_lookback_start_date,
+        datespine.date_day = user_fed.user_first_event_date as is_user_first_event_date,
+        datespine.mau_lookback_start_date >= user_fed.user_first_event_date as is_user_eligible_for_mau,
         events_dims_daily.*
     from datespine
     left join events_dims_daily
         on events_dims_daily.server_event_date between datespine.mau_lookback_start_date
            and datespine.date_day
+    left join user_fed
+        on events_dims_daily.user_id = user_fed.user_id
 )
 select *
 from 
     final
+where user_id = 499751 order by date_day, server_event_date
