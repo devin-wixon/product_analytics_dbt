fee85a21c866c77347f0601ec19ec8ccebf3179a
Devin Wixon <dwixon@frogstreet.com>
2025-10-03 11:54:29 -0500

    sqlfluff fix

--- models/marts/rpt_wau_mau_by_user.sql
+++ models/marts/rpt_wau_mau_by_user.sql
@@ -1,5 +1,5 @@
-with events_dims_daily as(
-    select 
+with events_dims_daily as (
+    select
         server_event_date,
         school_year_label,
         school_year_start_date,
@@ -17,25 +17,23 @@ with events_dims_daily as(
         district_state,
         is_distributed_demo_district,
         application_name
-    from    
-        {{ ref('rpt_obt_events_dims_daily') }}
+    from {{ ref('rpt_obt_events_dims_daily') }}
 ),
-user_fed as(
-    select 
+
+user_fed as (
+    select
         user_id,
         min(server_event_date) as user_first_event_date
-    from 
-        events_dims_daily
+    from events_dims_daily
     group by user_id
 ),
 
 datespine as (
-select
-    date_day,
-    mau_lookback_start_date,
-    wau_lookback_start_date
-from 
-    {{ ref('dim_day_datespine') }}
+    select
+        date_day,
+        mau_lookback_start_date,
+        wau_lookback_start_date
+    from {{ ref('dim_day_datespine') }}
 ),
 
 final as (
@@ -43,16 +41,35 @@ final as (
         datespine.date_day,
         datespine.mau_lookback_start_date,
         datespine.wau_lookback_start_date,
-        datespine.date_day = user_fed.user_first_event_date as is_user_first_event_date,
-        datespine.mau_lookback_start_date >= user_fed.user_first_event_date as is_user_eligible_for_mau,
-        events_dims_daily.*
+        datespine.date_day = user_fed.user_first_event_date
+            as is_user_first_event_date,
+        datespine.mau_lookback_start_date >= user_fed.user_first_event_date
+            as is_user_eligible_for_mau,
+        events_dims_daily.server_event_date,
+        events_dims_daily.school_year_label,
+        events_dims_daily.school_year_start_date,
+        events_dims_daily.user_id,
+        events_dims_daily.user_role,
+        events_dims_daily.event_category,
+        events_dims_daily.program_id,
+        events_dims_daily.program_name,
+        events_dims_daily.resource_id,
+        events_dims_daily.resource_title,
+        events_dims_daily.resource_type,
+        events_dims_daily.district_id,
+        events_dims_daily.district_name,
+        events_dims_daily.district_type,
+        events_dims_daily.district_state,
+        events_dims_daily.is_distributed_demo_district,
+        events_dims_daily.application_name
     from datespine
     left join events_dims_daily
-        on events_dims_daily.server_event_date between datespine.mau_lookback_start_date
-           and datespine.date_day
+        on events_dims_daily.server_event_date
+            between datespine.mau_lookback_start_date
+            and datespine.date_day
     left join user_fed
         on events_dims_daily.user_id = user_fed.user_id
 )
+
 select *
-from 
-    final
+from final
