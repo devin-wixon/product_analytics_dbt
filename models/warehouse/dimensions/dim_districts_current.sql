e09bd4403727567f87667e0348d5132c11a8c55b
Devin Wixon <dwixon@frogstreet.com>
2025-10-04 15:18:39 -0500

    dim_districts_current has settings

--- models/warehouse/dimensions/dim_districts_current.sql
+++ models/warehouse/dimensions/dim_districts_current.sql
@@ -2,24 +2,37 @@ with
 
 districts as (
     select
-        * exclude (
-            dbt_scd_id,
-            dbt_valid_from,
-            dbt_valid_to,
-            dbt_updated_at,
-            dbt_is_deleted
-        ),
+        *,
         {{ is_distributed_demo_district('district_id') }} as is_distributed_demo_district
     from {{ ref('stg_taco__districts') }}
     where
         dbt_valid_to is null
 ),
 
+district_settings as (
+    select
+        * exclude (
+            dbt_valid_from,
+            dbt_valid_to,
+            dbt_is_deleted)
+    from {{ ref('int_district_settings_history') }}
+    where
+        dbt_valid_to is null
+),
+
+joined as (
+    select
+        districts.*,
+        district_settings.* exclude (dbt_scd_id, district_id, district_settings)
+    from districts
+    left join district_settings
+        on districts.dbt_scd_id = district_settings.dbt_scd_id
+),
+
 final as (
     select *
-    from districts
+    from joined
 )
 
 select *
-from
-    final
+from final
