00f6cc3b031e6383459cb46bb9df17879d11c50b
Devin Wixon <dwixon@frogstreet.com>
2025-10-13 12:48:19 -0500

    remove applications; add path

--- models/marts/rpt_obt_events_dims_daily.sql
+++ models/marts/rpt_obt_events_dims_daily.sql
@@ -26,7 +26,10 @@ final as (
             visibility_status,
             user_invite_status,
             event_value,
-            event_path
+            path_entered,
+            path_left,
+            -- leaving app name out as so often accessed thru SSO; any counts wrong
+            application_name
         ),
         true as had_events_per_user_day_context,
         count(event_id) as n_events_per_user_day_context,
