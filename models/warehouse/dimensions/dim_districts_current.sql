a509884b74af7c39557d6080e8d347c9f8edb89b
Devin Wixon <dwixon@frogstreet.com>
2025-10-17 15:12:36 -0500

    fix diamond dag in districts

--- models/warehouse/dimensions/dim_districts_current.sql
+++ models/warehouse/dimensions/dim_districts_current.sql
@@ -4,34 +4,14 @@ districts as (
     select
         *,
         {{ is_distributed_demo_district('district_id') }} as is_distributed_demo_district
-    from {{ ref('stg_taco__districts') }}
+    from {{ ref('int_districts_history_parse_settings') }}
     where
         dbt_valid_to is null
-),
-
-district_settings as (
-    select
-        * exclude (
-            dbt_valid_from,
-            dbt_valid_to,
-            dbt_is_deleted)
-    from {{ ref('int_district_settings_history') }}
-    where
-        dbt_valid_to is null
-),
-
-joined as (
-    select
-        districts.*,
-        district_settings.* exclude (dbt_scd_id, district_id, district_settings)
-    from districts
-    left join district_settings
-        on districts.dbt_scd_id = district_settings.dbt_scd_id
 ),
 
 final as (
     select *
-    from joined
+    from districts
 )
 
 select *
