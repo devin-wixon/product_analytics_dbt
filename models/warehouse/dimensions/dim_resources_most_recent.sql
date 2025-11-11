3e82157fa8242554c1ba0395148d93deafaffd44
Devin Wixon <dwixon@frogstreet.com>
2025-11-11 13:28:49 -0600

    change most recent logic on resources

--- models/warehouse/dimensions/dim_resources_most_recent.sql
+++ models/warehouse/dimensions/dim_resources_most_recent.sql
@@ -7,10 +7,8 @@ resources as (
             dbt_updated_at
         )
     from {{ ref('stg_craft__resources') }}
-    qualify row_number() over (
-        partition by resource_id
-        order by dbt_valid_from desc
-    ) = 1
+    where
+        dbt_valid_to is null
 
 ),
 
