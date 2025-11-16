4d63cdb4179e455c2ac8cdee4567e0d65ed0fc2a
Devin Wixon <dwixon@frogstreet.com>
2025-11-16 12:15:41 -0600

    removing event information from program resource types

--- models/marts/rpt_program_resource_types.sql
+++ models/marts/rpt_program_resource_types.sql
@@ -17,18 +17,19 @@ resources as (
         {{ ref('dim_resources_most_recent') }}
 ),
 
+-- event dates not needed for current logic; keeping for use as needed
 -- get first and most recent dates for each resource id and program combination
 -- a resource-id is program-specific
-event_resource_id_agg as (
-    select
-        resource_id,
-        min(server_event_date) as first_event_date_resource_id,
-        max(server_event_date) as last_event_date_resource_id
-    from
-        {{ ref('fct_events') }}
-    group by
-        resource_id
-),
+-- event_resource_id_agg as (
+--     select
+--         resource_id,
+--         min(server_event_date) as first_event_date_resource_id,
+--         max(server_event_date) as last_event_date_resource_id
+--     from
+--         {{ ref('fct_events') }}
+--     group by
+--         resource_id
+-- ),
 
 joined as (
     select
@@ -39,13 +40,13 @@ joined as (
         || '-'
         || resources.resource_type as program_resource_type_id,
         count(resources.resource_id) as n_resources_in_program_resource_type,
-        min(event_resource_id_agg.first_event_date_resource_id)
-            as first_event_date_program_resource_type,
-        max(event_resource_id_agg.last_event_date_resource_id)
-            as last_event_date_program_resource_type
+        -- min(event_resource_id_agg.first_event_date_resource_id)
+        --     as first_event_date_program_resource_type,
+        -- max(event_resource_id_agg.last_event_date_resource_id)
+        --     as last_event_date_program_resource_type
     from resources
-    left join event_resource_id_agg
-        on resources.resource_id = event_resource_id_agg.resource_id
+    -- left join event_resource_id_agg
+    --     on resources.resource_id = event_resource_id_agg.resource_id
     left join programs
         on resources.resource_program_id = programs.program_id
     group by
