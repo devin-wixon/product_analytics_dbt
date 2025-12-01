6d7decd653260666106e5736e6c39c7a0a420728
Devin Wixon <dwixon@frogstreet.com>
2025-12-01 23:10:57 +0000

    fixes for linting

--- models/intermediate/int_events_enriched.sql
+++ models/intermediate/int_events_enriched.sql
@@ -3,7 +3,7 @@
     incremental_strategy='append'
 ) }}
 
--- set list of events that will be counted as planner path; excludes modal and launch events
+-- planner path events; excludes modal and launch events
 {% set planner_path_events = "(
     'weekly.planner.program.week.filter.close',
     'weekly.planner.program.week.filter.deselect.all',
@@ -45,7 +45,7 @@ with events as (
             {% if is_incremental() %}
                 (select max(dbt_row_batch_id) + 1 from {{ this }} )
             {% else %}
-            0
+                0
             {% endif %}
             , 38, 0
         ) as dbt_row_batch_id
@@ -59,66 +59,70 @@ with events as (
 
 ),
 
-event_metadata as
-(select
-    * exclude (proposed_new_event_name)
-from
-    {{ ref('seed_event_log_metadata') }}
+event_metadata as (
+    select * exclude (proposed_new_event_name)
+    from
+        {{ ref('seed_event_log_metadata') }}
 
 ),
 
-joined as
-(select
-    events.*,
-    event_metadata.* exclude (event_name)
-from
-    events
-left join 
-    event_metadata 
-on 
-    -- left join ensures completeness
-    -- relationship test: event_name FK is a warn status in the events staging model
-    events.event_name = event_metadata.event_name
+joined as (
+    select
+        events.*,
+        event_metadata.* exclude (event_name)
+    from
+        events
+    left join
+        event_metadata
+        on
+            -- left join ensures completeness
+            -- but relationship test is in the events staging model
+            events.event_name = event_metadata.event_name
 ),
 
 -- cleaning and add info for events or values with unique behaviors
 events_add_column_info as (
     select
         joined.*,
-        date(server_timestamp) as server_event_date,
-        date(client_timestamp) as client_event_date,
+        date(joined.server_timestamp) as server_event_date,
+        date(joined.client_timestamp) as client_event_date,
         -- events with unique behaviors
-        case 
+        case
             -- router.left events: 
             -- path = route user navigating TO
             -- event_value = route user just LEFT
-            when event_name = 'router.left' then event_path 
-            when event_name = 'router.enter' then event_value 
-            else null 
+            when joined.event_name = 'router.left' then joined.event_path
+            when joined.event_name = 'router.enter' then joined.event_value
         end as path_entered,
-        case when event_name = 'router.left' then event_value 
-            else null 
+        case
+            when joined.event_name = 'router.left' then joined.event_value
         end as path_left,
-        
-        -- this event joins to the focuses_areas.value (source name) text rather than the integer ID
-        case when event_name in (
-            'weekly.planner.program.week.filter.deselect', 
-            'weekly.planner.program.week.filter.select')
-                then event_value_human_readable 
-            else null 
+
+        -- joins to the focuses_areas.value (source name) text
+        case
+            when
+                joined.event_name in (
+                    'weekly.planner.program.week.filter.deselect',
+                    'weekly.planner.program.week.filter.select'
+                )
+                then joined.event_value_human_readable
         end as focus_area_label,
 
-        -- extract info such as week_number from planner paths for specific events
+        -- extract info such as week_number from planner paths
         -- TAG IMPROVEMENT
-        -- extract theme vs month_number: join to resource_id when it's not a month number
-        -- code is regexp_substr(events.event_path, '^/planner/[^/]+/([^/]+)', 1, 1, 'e', 1)    
-        case 
-            when event_name in {{ planner_path_events }}
-            then try_cast(
-                regexp_substr(
-                    event_path, '^/planner/[^/]+/[^/]+/([0-9]+)$', 1, 1, 'e', 1
-                    ) as integer)
-            else null
+        -- extract theme vs month_number: join to resource_id when not a month 
+        case
+            when joined.event_name in {{ planner_path_events }}
+                then try_cast(
+                    regexp_substr(
+                        joined.event_path,
+                        '^/planner/[^/]+/[^/]+/([0-9]+)$',
+                        1,
+                        1,
+                        'e',
+                        1
+                    ) as integer
+                )
         end as week_number,
 
         -- program_id may be in path but not in event_value
@@ -126,14 +130,21 @@ events_add_column_info as (
         -- if a user copy/pastes a path value that isn't numeric, it will null
         try_cast(
             coalesce(
-                case when event_value_joins_to = 'program_id' 
-                    and try_cast(event_value as integer) 
-                    then event_value 
-                    else null 
+                case
+                    when
+                        joined.event_value_joins_to = 'program_id'
+                        and try_cast(joined.event_value as integer)
+                        then joined.event_value
                 end,
-                regexp_substr(event_path, 'resources/([^/]+)', 1, 1, 'e', 1),
-                regexp_substr(event_path, 'planner/([^/]+)', 1, 1, 'e', 1),
-                regexp_substr(event_value, 'resources/([^/]+)', 1, 1, 'e', 1)
+                regexp_substr(
+                    joined.event_path, 'resources/([^/]+)', 1, 1, 'e', 1
+                ),
+                regexp_substr(
+                    joined.event_path, 'planner/([^/]+)', 1, 1, 'e', 1
+                ),
+                regexp_substr(
+                    joined.event_value, 'resources/([^/]+)', 1, 1, 'e', 1
+                )
             ) as integer
         ) as program_id,
 
@@ -141,62 +152,71 @@ events_add_column_info as (
         -- for router.left this will be the resource TO, not the one left
         try_cast(
             coalesce(
-                case when 
-                    event_value_joins_to = 'resource_id' 
-                    and try_cast(event_value as integer) 
-                    then event_value 
-                    else null 
+                case
+                    when
+                        joined.event_value_joins_to = 'resource_id'
+                        and try_cast(joined.event_value as integer)
+                        then joined.event_value
                 end,
-                regexp_substr(event_path, 'detail/([0-9]+)', 1, 1, 'e', 1),
-                regexp_substr(event_value, 'detail/([0-9]+)', 1, 1, 'e', 1)
+                regexp_substr(
+                    joined.event_path, 'detail/([0-9]+)', 1, 1, 'e', 1
+                ),
+                regexp_substr(
+                    joined.event_value, 'detail/([0-9]+)', 1, 1, 'e', 1
+                )
             ) as integer
-    ) as resource_id
+        ) as resource_id
 
     from
         joined
 ),
 
-events_add_pivots as
-(select
-    -- do not follow with a comma; conditional comma below
-    events_add_column_info.*
+events_add_pivots as (
+    select
+        -- do not follow with a comma; conditional comma below
+        events_add_column_info.*
+
     
-    -- columns that should be an integer and are a foreign key
     {%- set id_join_columns = dbt_utils.get_column_values(
         ref('seed_event_log_metadata'), 'event_value_joins_to') -%}
 
     -- columns that will be a string and don't join to a key
     {%- set not_id_columns = dbt_utils.get_column_values(
         ref('seed_event_log_metadata'), 'event_value_not_id') %}
-    
-    -- cast as integer needed for foreign keys
-    {% if id_join_columns -%}
-        {%- for col in id_join_columns -%}
-            {%- if col and col not in manually_added_columns -%}
-                ,
-                case
-                    when event_value_joins_to = '{{ col }}'
-                        then try_cast(event_value as integer)
-                    else null
-                end as {{ col }}
-            {%- endif -%}
-        {%- endfor -%}
-    {%- endif -%}
 
-    -- non-foreign key
-    -- rest of loop is identical
-    {% if not_id_columns -%}
-        {%- for col in not_id_columns -%}
-            {%- if col and col not in manually_added_columns -%}
-                ,
-                case
-                    when event_value_not_id = '{{ col }}'
-                        then event_value
-                    else null
-                end as {{ col }}
-          {%- endif -%}
-      {%- endfor -%}
-  {%- endif -%}
+    -- cast as integer needed for foreign keys
+        {% if id_join_columns -%}
+            {%- for col in id_join_columns -%}
+                {%- if col and col not in manually_added_columns -%}
+                    ,
+                    case
+                        when
+                            events_add_column_info.event_value_joins_to
+                            = '{{ col }}'
+                            then
+                                try_cast(
+                                    events_add_column_info.event_value as integer
+                                )
+                    end as {{ col }}
+                {%- endif -%}
+            {%- endfor -%}
+        {%- endif -%}
+
+        -- non-foreign key
+        -- rest of loop is identical
+        {% if not_id_columns -%}
+            {%- for col in not_id_columns -%}
+                {%- if col and col not in manually_added_columns -%}
+                    ,
+                    case
+                        when
+                            events_add_column_info.event_value_not_id
+                            = '{{ col }}'
+                            then events_add_column_info.event_value
+                    end as {{ col }}
+                {%- endif -%}
+            {%- endfor -%}
+        {%- endif -%}
 
     -- add boolean is_ columns for event categories
     -- code works, not currently used; commented out
@@ -215,34 +235,33 @@ events_add_pivots as
     --       {%- endfor %}
     --   {%- endif %}
     #}
- from
-    events_add_column_info
+    from
+        events_add_column_info
 ),
 
-final as
-(select
-   events_add_pivots.* exclude (
+final as (
+    select
+        events_add_pivots.* exclude (
         -- event_path,
         -- event_value,
-        event_value_joins_to,
-        event_value_not_id,
-        notes,
-        event_trigger,
-        handled_as_exception_in_code,
-        example_values,
-        event_capture_end_date,
-        event_capture_start_date
+            event_value_joins_to,
+            event_value_not_id,
+            notes,
+            event_trigger,
+            handled_as_exception_in_code,
+            example_values,
+            event_capture_end_date,
+            event_capture_start_date
         ),
-    {{ assign_event_type('event_category') }} as event_type
+        {{ assign_event_type('event_category') }} as event_type
 
     -- don't persist ETL
     -- _source_filename,
     -- _loaded_at_utc
-from
-    events_add_pivots
+    from
+        events_add_pivots
 )
 
-select
-    *
+select *
 from
     final
