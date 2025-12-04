08038e497883c57e4bedd9451eaa29f15c3a432e
Devin Wixon <dwixon@frogstreet.com>
2025-12-04 16:26:26 -0600

    remove is_district_enabled from dim models

--- models/warehouse/dimensions/dim_districts_most_recent.sql
+++ models/warehouse/dimensions/dim_districts_most_recent.sql
@@ -2,7 +2,7 @@ with
 
 districts as (
     select
-        *,
+        * exclude (is_district_enabled),
         {{ is_distributed_demo_district('district_id') }} as is_distributed_demo_district
     from {{ ref('int_districts_history_parse_settings') }}
     where
